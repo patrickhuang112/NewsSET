d29ea98ecd0034188d605644f7a8cc54c04d7f09
Patrick Huang <pat.b.huang@gmail.com>
2020-10-21 23:02:17 -0700

    added new user facing formatting features

--- static/page.js
+++ static/page.js
@@ -17,6 +17,12 @@ const searchType = {
     SOURCES : 'sources'
 }
 
+const endings = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th']
+
+const monthNames = ["January", "February", "March", "April", "May", "June", 
+                    "July", "August", "September", "October", "November",
+                    "December"]
+
 // Search elements
 const searchbar = document.getElementById('searchbar');
 const submit = document.getElementById('submit');
@@ -26,6 +32,8 @@ const articleList = document.getElementById('article-list');
 const radioENT = document.getElementById('ent');
 const radioTCH = document.getElementById('tch');
 const radioSPT = document.getElementById('spt');
+
+let currentArticleList = new Array();
 
 class Article {
     constructor(author, publisher, title, description, date, url, image=null) {
@@ -58,12 +66,15 @@ submit.onclick = () => {
     
     console.log('Search performed')
 
-
     // Visually reset searchbar value
     setSearchbarValue('');
 };
 
-function clearArticles() {
+function clearModelArticles() {
+    currentArticleList = new Array(); 
+}
+
+function clearViewArticles() {
     while(articleList.firstChild != null) {
         articleList.removeChild(articleList.firstChild);
     }
@@ -71,26 +82,72 @@ function clearArticles() {
 
 function parseNewArticles(articles, limit = 5){
     for (let i = 0; i < limit; i++) {
-        parsedDiv = parseArticle(articles[i]);
+        currentArticleList.push(articles[i]);
+
+        let parsedDiv = parseArticle(articles[i]);
         let li = document.createElement('li');
+
         li.appendChild(parsedDiv); 
         articleList.appendChild(li);
     } 
     if (articleList.childElementCount > 10) {
-        console.log('here')
         clearArticles();
     }
 }
 
-function parseArticle(article){
-    p = document.createElement('p');
-    p.textContent = 'TESTING';
-    return p;
+function sortArticlesByDate (latest) {
+    let sortFunc = ((a,b) => {
+        if (a < b) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    
+    if(latest) {
+        currentArticleList.sort(sortFunc).reverse()
+    } else {
+        currentArticleList.sort(sortFunc);
+    }
+}
+
+    
+function parseArticle(article) {
+    let newDiv = document.createElement('div');
+    let userFriendlyDate = getUserFriendlyDate(article.publishedAt);
+    newDiv.innerHTML = `
+    <h3>"${article.title}"</h3> 
+    <b>Author</b>: ${article.author}<br> 
+    <b>Date Published</b>: ${userFriendlyDate}<br> 
+    <b>Website</b>: ${article.source.name} <br>
+    <p> Summary: ${article.description} </p> 
+    <p> Interested? Read more <a href="${article.url}">here</a></p>`
+    
+    return newDiv;
+}
+
+function getUserFriendlyDate(date) {
+    let dateObj = new Date(date);
+    let monthName = monthNames[dateObj.getMonth()];
+    let day = dateObj.getDate().toString();
+    let dayEnding = endings[dateObj.getDate() % 10];
+    let year = dateObj.getFullYear();
+
+    let ampm = dateObj.getHours() > 11 ? 'pm' : 'am'; 
+    let hour = dateObj.getHours() % 12 == 0 ? '12' : (dateObj.getHours() % 12).toString();
+    let mins = dateObj.getMinutes() == 0 ? "00" : dateObj.getMinutes().toString();
+
+    // Ex: June 4th, 2020 at 4:20pm
+    return  `${monthName} ${day}${dayEnding}, ${year} at ${hour}:${mins}${ampm}`;
+}
+
+function getUserFriendlyMonth(month) {
+
 }
 
 function getURL(category, kword = '', type = searchType.TOP, country = 'us') {
     if (category != searchCategory.NONE) {
-        url = `${baseURL}${type}?category=${category}&apikey=${apiKey}`;
+        url = `${baseURL}${type}?country=${country}&category=${category}&apikey=${apiKey}`;
         return url
     } else {
         return url = `${baseURL}${type}?country=${country}&apikey=${apiKey}`;
